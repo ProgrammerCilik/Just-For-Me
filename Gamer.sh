4bef74c9b4c9033dafb8e515229c3635832232b1
AkhmadZiyad <64240233+ProgrammerCilik@users.noreply.github.com>
2020-07-19 11:54:53 +0800

    Add files via upload

---
+++ Gamer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+clear
+figlet Skoletch HD | lolcat
+
+echo "====================================================" | lolcat
+echo -n "= Waktu Sekarang  : " | lolcat  ; date    	    | lolcat   
+echo -n "= Users           : " | lolcat  ; whoami	    | lolcat   
+echo "====================================================" | lolcat
