1dd377dc8bd8d2bdbf46576e618cad48b44b543a
AkhmadZiyad <64240233+ProgrammerCilik@users.noreply.github.com>
2020-07-20 11:09:38 +0800

    Update Gamer.sh

--- Gamer.sh
+++ Gamer.sh
@@ -2,8 +2,4 @@
 
 clear
 figlet Skoletch HD | lolcat
-
-echo "====================================================" | lolcat
-echo -n "= Waktu Sekarang  : " | lolcat  ; date    	    | lolcat   
-echo -n "= Users           : " | lolcat  ; whoami	    | lolcat   
 echo "====================================================" | lolcat
